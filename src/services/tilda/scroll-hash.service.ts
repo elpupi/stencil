d66b93253c2e31e222e89b5193429f5d779d7205
Thomas Milotti <thomas.milotti@gmail.com>
2022-02-01 18:11:19 +0100

    enhance scroll-hash service but removed from service because it is well handled now by Tilda

--- src/services/tilda/scroll-hash.service.ts
+++ src/services/tilda/scroll-hash.service.ts
@@ -1,44 +1,169 @@
-export const loadScrollHashService = () => {
-    $(() => {
-        const links = $('a[href^="#rec"]').toArray();
+export type ScrollHashOptions = {
+    menuHeight: () => number;
+};
 
-        for (const link of links) {
-            $(link).off('click'); // we remove previous click handler from jquery to replace with a better solution
+export const loadScrollHashService = (options: ScrollHashOptions) => {
+    const { menuHeight } = options;
 
-            link.addEventListener('click', e => {
-                e.preventDefault();
+    const scrollTo = (hash: string, scrollOptions: /* ScrollToOptions & */ { speed?: number; offset?: number; } = {}) => {
+        const target = document.querySelector(hash);
 
-                const targetHref = link.getAttribute('href');
-                const target = document.querySelector(targetHref);
-
-                if (!target) {
-                    console.warn(`the link with href="${link.getAttribute('href')}" has no targets node with id="${targetHref}"`);
-                    return;
-                }
-
-                const menuHeight = $(window).width() > 980 ? 80 : 60;
-                const top = target.getBoundingClientRect().top - menuHeight;
-
-                window.scrollBy({
-                    top,
-                    left: 0,
-                    behavior: 'smooth'
-                });
-            });
-
-            // because Tilda set a jQuery click event on the button that will override ours
-            // So we add ours and delete the addEventListener listener. (TO ENHANCE WITH A BETTER SOLUTION)
-            // now we prevent Tilda to add 'click' event if it is done afterwards
-            const oldListener = link.addEventListener.bind(link);
-
-            link.addEventListener = (...args: Parameters<Element[ 'addEventListener' ]>): ReturnType<Element[ 'addEventListener' ]> => {
-                if (args[ 0 ] === 'click') {
-                    // skip
-                } else {
-                    return oldListener(...args);
-                }
-            };
-            // link.addEventListener = () => { };
+        if (!target) {
+            console.warn(`no targets node with id="${hash.replace(/^#/, '')}"`);
+            return;
         }
+
+        // const menuHeight = $(window).width() > 980 ? 80 : 60;
+        // const top = target.getBoundingClientRect().top - menuHeight();
+
+        /* window.scrollBy({
+            top,
+            left: 0,
+            behavior: 'smooth',
+            ...scrollOptions
+        }); */
+        t270_scroll(hash, scrollOptions.offset ?? menuHeight(), scrollOptions.speed || 400);
+    };
+
+
+
+    // const setInternalPageHashLinks = () => {
+    //     const links = $('a[href^="#rec"]').toArray() as HTMLAnchorElement[];
+
+    //     for (const link of links) {
+    //         $(link).off('click'); // we remove previous click handler from jquery to replace with a better solution
+
+    //         link.addEventListener('click', e => {
+    //             e.preventDefault();
+    //             scrollTo(link.href);
+    //         });
+
+    //         // because Tilda set a jQuery click event on the button that will override ours
+    //         // So we add ours and delete the addEventListener listener. (TO ENHANCE WITH A BETTER SOLUTION)
+    //         // now we prevent Tilda to add 'click' event if it is done afterwards
+    //         /* const oldListener = link.addEventListener.bind(link);
+
+    //         link.addEventListener = (...args: Parameters<Element[ 'addEventListener' ]>): ReturnType<Element[ 'addEventListener' ]> => {
+    //             if (args[ 0 ] === 'click') {
+    //                 // skip
+    //             } else {
+    //                 return oldListener(...args);
+    //             }
+    //         }; */
+    //         // link.addEventListener = () => { };
+    //     }
+    // };
+
+    const initHashLinks = (window: { pathname: string; url: string; hash: string; }) => {
+
+        const selectors = [
+            `a[href^="#"]:not(${[
+                '[href="#"]',
+                '.carousel-control',
+                '.t-carousel__control',
+                '[href^="#price"]',
+                '[href^="#popup"]',
+                '[href^="#prodpopup"]',
+                '[href^="#order"]',
+                'a[href^="#!"]' ].join(',')})`,
+
+            `a[href^="${window.pathname}#"]:not(${[
+                'a[href*="#!/tproduct/"]',
+                'a[href*="#!/tab/"]',
+                '[href*="#popup"]' ].join(',')})`,
+
+            `a[href^="${window.url}#"]:not(${[
+                'a[href*="#!/tproduct/"]',
+                'a[href*="#!/tab/"]',
+                '[href*="#popup"]' ].join(',')})`
+        ].join(',');
+
+
+        $(selectors).on('click', function (this: HTMLAnchorElement, e) {
+            e.preventDefault();
+            const hash = this.hash.trim();
+            scrollTo(hash);
+        });
+
+
+        if ($('.js-store').length > 0 || $('.js-feed').length > 0)
+            scrollTo(window.hash, { speed: 1 });
+    };
+
+    const initLinksWithNameAttribute = (window: { hash: string; }) => {
+        const { hash } = window;
+        const hasElementWithHashName = () => hash && $('a[name="' + hash.slice(1) + '"]').length > 0;
+
+        setTimeout(() => {
+            if (hasElementWithHashName())
+                scrollTo(hash);
+        }, 1000);
+
+        $(window).on('popstate', _event => {
+            if (hasElementWithHashName())
+                scrollTo(hash);
+        });
+    };
+
+    $(() => {
+
+        const { pathname, href: url, hash } = window.location;
+
+        if (hash)
+            scrollTo(hash/* , { behavior: 'auto' } */);
+
+        // this is taken from the block t270 initilization <script>...</script>
+        setTimeout(() => initHashLinks({ pathname, url, hash }), 500);
+        initLinksWithNameAttribute({ hash });
     });
 };
+
+
+
+
+declare let t270_scroll: (hash: string, offset: number, speed?: number) => void;
+
+/*
+function t270_scroll(hash, offset, speed) {
+    if (hash.indexOf('#!/tproduct/') !== -1 || hash.indexOf('#!/tab/') !== -1) {
+        return !0;
+    }
+    var root = $('html, body');
+    var target = "";
+    if (speed === undefined) {
+        speed = 400;
+    }
+    try {
+        target = $(hash);
+    } catch (event) {
+        console.log("Exception t270: " + event.message);
+        return !0;
+    }
+    if (target.length === 0) {
+        target = $('a[name="' + hash.substr(1) + '"]');
+        if (target.length === 0) {
+            return !0;
+        }
+    }
+    var isHistoryChangeAllowed = window.location.hash !== hash;
+    var complete = function () {
+        if (!isHistoryChangeAllowed) {
+            return;
+        }
+        if (history.pushState) {
+            history.pushState(null, null, hash);
+        } else {
+            window.location.hash = hash;
+        }
+        isHistoryChangeAllowed = !1;
+    };
+    var dontChangeHistory = Boolean($('.t270').attr('data-history-disabled'));
+    if (dontChangeHistory) {
+        complete = function () { };
+    }
+    root.animate({
+        scrollTop: target.offset().top - offset
+    }, speed, complete);
+    return !0;
+};
+ */
--- src/services/tilda/tilda-services.module.ts
+++ src/services/tilda/tilda-services.module.ts
@@ -2,14 +2,14 @@ import { LoadServices } from '@upradata/browser-util';
 import { LoadingAnimationPopup } from './loading-animation-popup.service';
 import { LanguageService } from './language.service';
 import { Popup } from './popup.service';
-import { loadScrollHashService } from './scroll-hash.service';
+// import { loadScrollHashService } from './scroll-hash.service';
 import { loadIsMobileService } from './is-mobile.service';
 import { TildaModuleServices, TildaModuleServicesOpts } from './types';
 
 
 export const loadServices: LoadServices<TildaModuleServicesOpts, TildaModuleServices> = servicesConfig => {
     loadIsMobileService(); // event base service with nothing to be returned
-    loadScrollHashService(); // event base service with nothing to be returned
+    // loadScrollHashService(); // event base service with nothing to be returned
 
     const popup = new Popup(servicesConfig.popup);
     const loadingAnimationPopup = new LoadingAnimationPopup(servicesConfig.loadingAnimationPopup, popup);
