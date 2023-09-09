747334fe91f94cbc3feeb1711d60404104fe015f
El Pupi <a01633814@unet.univie.ac.at>
2023-09-09 13:16:51 +0200

    computer reinstallation files metadata changed

--- src/services/tilda/tilda-zero-block.service.ts
+++ src/services/tilda/tilda-zero-block.service.ts
@@ -7,58 +7,90 @@ export type OnZeroBlockMouseHoverOptions = {
     componentHoverCssName?: string;
     onEnter?: (options: OnListenerOptions) => void;
     onLeave?: (options: OnListenerOptions) => void;
+    onComponentEnter?: (options: OnListenerOptions) => void;
+    onComponentLeave?: (options: OnListenerOptions) => void;
 };
 
 
 
-const getParentFrom = (from: HTMLElement, accept: (el: HTMLElement) => boolean): HTMLElement | null => {
-    return !from || accept(from) ? from : getParentFrom(from.parentElement, accept);
+const getParent = (from: HTMLElement, accept: (el: HTMLElement) => boolean): HTMLElement | null => {
+    return !from || accept(from) ? from : getParent(from.parentElement, accept);
 };
 
-export const getParentTildaBlock = (from: HTMLElement) => getParentFrom(from, el => !!el?.id);
-export const getParentZeroBlockElement = (from: HTMLElement) => getParentFrom(from, el => el?.classList.contains('tn-elem'));
+export const getTildaBlock = (from: HTMLElement) => getParent(from, el => !!el?.id);
+export const getZeroBlockElement = (from: HTMLElement) => getParent(from, el => el?.classList.contains('tn-elem'));
 
 export const onZeroBlockMouseHover = (elements: HTMLElement[], options: OnZeroBlockMouseHoverOptions = {}) => {
     const {
         componentCssName: cmpCssName,
-        componentElements,
+        componentElements, // elements forming the component
         elementHoverCssName = 'mt-element-hover',
         componentHoverCssName = 'mt-component-hover',
         onEnter,
-        onLeave
+        onLeave,
+        onComponentEnter,
+        onComponentLeave
     } = options;
 
+    // elements to listen to
     const elts = Array.isArray(elements) ? elements : [ elements ];
     const componentCssName = cmpCssName || [ ...elts[ 0 ].classList ].find(name => name.startsWith('mt-'));
 
+    let state: 'cmp-hovered' | 'cmp-not-hovered' = 'cmp-not-hovered';
+
     const addListener = (element: HTMLElement) => {
-        const tildaBlock = getParentTildaBlock(element);
+        const tildaBlock = getTildaBlock(element);
         const componentElts = componentElements || [ ...tildaBlock.querySelectorAll<HTMLElement>(`.${componentCssName}`) ];
 
+        // mouse enters element
         element.addEventListener('mouseenter', () => {
-            if (!element.classList.contains(elementHoverCssName)) {
+            element.classList.add(elementHoverCssName);
+
+            if (state === 'cmp-not-hovered') {
                 componentElts.forEach(el => el.classList.add(componentHoverCssName));
-                element.classList.add(elementHoverCssName);
-                onEnter?.({ element, componentElts });
+                onComponentEnter?.({ element, componentElts });
+                state = 'cmp-hovered';
+            }
+
+            onEnter?.({ element, componentElts });
+        });
+
+        // We can have stacked elements, or nested ones, or intersecting ones
+        // So while we are hovering one element, we can enter and leave other ones
+        // We could keep a counter but it is easier to keep in the closure the entered "element".
+        // We can then listen to the other component elements 
+        const addMouseLeave = (elt: HTMLElement) => elt.addEventListener('mouseleave', () => {
+            // get the highest "Z" element hovered
+            const hoveredElmt = [ ...document.querySelectorAll<HTMLElement>(':hover') ]?.at(-1);
+            // get the tn-elem being the ZeroBlock wrapper of each element
+            const hoveredZeroBlockElmt = getZeroBlockElement(hoveredElmt);
+            // check if the wrapper is hovered
+            const isComponentEltHovered = componentElts.some(el => el === hoveredZeroBlockElmt);
+
+            // we continue just if "element" is hovered also
+            if (element.classList.contains(elementHoverCssName)) {
+                // the element hovered is not a component elt
+                if (!isComponentEltHovered)
+                    componentElts.forEach(el => el.classList.remove(componentHoverCssName));
+
+                const eltsMinusElement = elts.filter(el => el !== element);
+                const otherEltIsHovered = eltsMinusElement.some(el => el === hoveredZeroBlockElmt);
+
+                // the element hovered is not a component elt OR another elt than "element" is hovered
+                if (!isComponentEltHovered || otherEltIsHovered) {
+                    element.classList.remove(elementHoverCssName);
+                    onLeave?.({ element, componentElts });
+                }
+
+                // the element hovered is not a component elt
+                if (!isComponentEltHovered && state === 'cmp-hovered') {
+                    onComponentLeave?.({ element, componentElts });
+                    state = 'cmp-not-hovered';
+                }
+
             }
         });
 
-        const addMouseLeave = (elt: HTMLElement) => {
-            elt.addEventListener('mouseleave', () => {
-                const hoveredElmt = [ ...document.querySelectorAll<HTMLElement>(':hover') ]?.at(-1);
-                const hoveredZeroBlockElmt = getParentZeroBlockElement(hoveredElmt);
-                const isHoverComponentElmt = componentElts.some(el => el === hoveredZeroBlockElmt);
-
-                if (element.classList.contains(elementHoverCssName)) {
-                    if (!isHoverComponentElmt)
-                        componentElts.forEach(el => el.classList.remove(componentHoverCssName));
-                    if (!isHoverComponentElmt || elts.filter(el => el !== element).some(el => el === hoveredZeroBlockElmt))
-                        element.classList.remove(elementHoverCssName);
-                    if (!isHoverComponentElmt)
-                        onLeave?.({ element, componentElts });
-                }
-            });
-        };
 
         componentElts.forEach(addMouseLeave);
     };
